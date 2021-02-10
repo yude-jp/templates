55e1f758d96b902c0f6ae9f3d588c292b5e78cb9
yudete <yu@yude.moe>
2021-02-10 15:34:56 +0900

    リンク、文章を更新

---
+++ discord/welcome.md
@@ -0,0 +1,15 @@
+# [yude.jp の Discord サーバーへようこそ!](https://yude.jp)
+Welcome to yude.jp Discord server!
+
+## 💫 概要
+このサーバーの目的は特にありません。
+Member ロールを付与されると、すべてのチャンネルが閲覧・書き込みできるようになります。
+ロール付与は現在手動ですから、管理者が操作を行うまでしばらくお待ちください。
+招待リンク: [discord.gg/X6srY7X](https://discord.gg/X6srY7X)
+
+## 📕 ルール
+1. 過激すぎる発言をしないでください。
+2. チャットスパムを行わないでください。
+
+## 📎 リンク
+[GitHub Organization](https://github.com/yudemoe), [Twitter](https://twitter.com/yude_jp), [LINE オープンチャット](https://line.me/ti/g2/mi36ZEsJkIo9BLqYg1sQFg)
---
+++ discord/welcome.py
@@ -0,0 +1,22 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('{0.user} としてログインしました。'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('!welcome'):
+        embed=discord.Embed(title="yude.jp の Discord サーバーへようこそ!", url="https://yude.jp", description="Welcome to yude.jp Discord server!", color=0xffa3f0)
+        embed.set_thumbnail(url="https://cdn.discordapp.com/icons/550309736214691840/2e18e529b75fc216f970b59a067bd737.webp?size=128")
+        embed.add_field(name="💫 概要", value="このサーバーの目的は特にありません。\nMember ロールを付与されると、すべてのチャンネルが閲覧・書き込みできるようになります。\nロール付与は現在手動ですから、管理者が操作を行うまでしばらくお待ちください。\n招待リンク: [discord.gg/X6srY7X](https://discord.gg/X6srY7X)", inline=False)
+        embed.add_field(name="📕 ルール", value="1. 過激すぎる発言をしないでください。\n2. チャットスパムを行わないでください。", inline=False)
+        embed.add_field(name="📎 リンク", value="[GitHub Organization](https://github.com/yudemoe), [Twitter](https://twitter.com/yude_jp), [LINE オープンチャット](https://line.me/ti/g2/mi36ZEsJkIo9BLqYg1sQFg)", inline=False)
+
+        await message.channel.send(embed=embed)
+
+client.run('token')
